43d8d72c247b60e8114632c3cfbdf11b058fd1ad
João Guedes <jotagw@gmail.com>
2023-11-13 00:43:01 -0300

    Curva roc e simplificação de fatores

--- data_modelling.R
+++ data_modelling.R
@@ -6,6 +6,7 @@ library(gamlss)
 library(woe)
 #install.packages("xtable")
 library(xtable)
+library(tidyverse)
 
 # Leitura de Dados ----
 dt <- fread("./data/heart.csv")
@@ -25,7 +26,44 @@ print(lapply(lapply(dt, unique),sort))
 dt <- dt[RestingBP != 0 & Cholesterol !=0 & Oldpeak > 0] # https://d-nb.info/1242792767/34
 
 # Tranformacao para variavel categorica ----
-dt[,c(2,3,6,7,9,11,12)] <- lapply(dt[,c(2,3,6,7,9,11,12)], as.factor)
+# dt[,c(2,3,6,7,9,11,12)] <- lapply(dt[,c(2,3,6,7,9,11,12)], as.factor)
+
+
+#Simplify ChestPainType, only check for Asymptomatic pain
+dt$ChestPainASY <- 0
+dt$ChestPainASY[dt$ChestPainType == "ASY"] <- 1
+dt$ChestPainASY <- as.factor(dt$ChestPainASY)
+
+#Simplify RestingECG, only check for ST
+dt$RestingECGST <- 0
+dt$RestingECGST[dt$RestingECG == "ST"] <- 1
+dt$RestingECGST <- as.factor(dt$RestingECGST)
+
+#Simplify ST_Slope, only check for Down/Flat
+dt$ST_SlopeDownFlat <- 1
+dt$ST_SlopeDownFlat[dt$ST_Slope == "Up"] <- 0
+dt$ST_SlopeDownFlat <- as.factor(dt$ST_SlopeDownFlat)
+
+#Turn Sex into factor
+dt$Sex_fct <- 1
+dt$Sex_fct[dt$Sex == "F"] <- 0
+dt$Sex_fct <- as.factor(dt$Sex_fct)
+
+dt$ExerciseAngina_fct <- 1
+dt$ExerciseAngina_fct[dt$ExerciseAngina == "N"] <- 0
+dt$ExerciseAngina_fct <- as.factor(dt$ExerciseAngina_fct)
+
+removecols <- c("ChestPainType","Cholesterol_imp","RestingBP_imp","Oldpeak_imp","RestingECG","ST_Slope","ExerciseAngina","Sex")
+dt <- dt[,c("Age","RestingBP", "Cholesterol", "FastingBS", "MaxHR", "Oldpeak",
+            "HeartDisease", "ChestPainASY", "RestingECGST", 
+            "ST_SlopeDownFlat", "Sex_fct", "ExerciseAngina_fct")]
+#names(dt)[!names(dt) %in% removecols]
+#Dataset is looking better
+#Add heartdisease back to the data frame
+#heart_imp$HeartDisease <- heart$HeartDisease
+
+
+
 
 # Selecao de variaveis (IV e GAIC)
 iv_age <- woe(Data=dt, Independent="Age", Continuous=TRUE, Dependent="HeartDisease", C_Bin=10, Bad=0, Good=1)[,c("MIN", "MAX", "IV")]
@@ -57,24 +95,30 @@ iv_sum <- data.frame(
 print(xtable(iv_sum, type = "latex"), file = "iv_sum.tex")
 
 
+
+
+
+
+
+
 #woe(Data=mtcars,"cyl",FALSE,"am",10,Bad=0,Good=1)
-model <- gamlss(HeartDisease ~ Sex*ChestPainType, data = dt, family = BI(mu.link=probit))
+model <- gamlss(HeartDisease ~ Sex_fct*ChestPainASY, data = dt, family = BI(mu.link=logit))
 gamlss::stepGAIC(model,
                  scope = c(lower = ~ 1,
                            upper = ~ Age + RestingBP + FastingBS + 
-                           RestingECG + MaxHR + ExerciseAngina + 
-                           Oldpeak + ST_Slope + Sex*ChestPainType),
+                           RestingECGST + MaxHR + ExerciseAngina_fct + 
+                           Oldpeak + ST_SlopeDownFlat + Sex_fct*ChestPainASY),
                  direction = "both")
 
 
 
-step_variables <- c("Sex", "ChestPainType", "Oldpeak", "ST_Slope", 
-                    "ExerciseAngina", "Age", "RestingBP", "HeartDisease")
-heart_step <- dt[,c("Sex", "ChestPainType", "Oldpeak", "ST_Slope",
-                    "ExerciseAngina", "Age", "RestingBP", "HeartDisease")]
+step_variables <- c("Sex_fct", "ChestPainASY", "Oldpeak", "ST_SlopeDownFlat", 
+                    "ExerciseAngina_fct", "Age", "RestingBP", "HeartDisease")
+heart_step <- dt[,c("Sex_fct", "ChestPainASY", "Oldpeak", "ST_SlopeDownFlat",
+                    "ExerciseAngina_fct", "Age", "RestingBP", "HeartDisease")]
 
 step_model <- gamlss(formula = HeartDisease ~ .,  
-                     family = BI(mu.link = probit), data = heart_step, trace = FALSE)
+                     family = BI(mu.link = logit), data = heart_step, trace = FALSE)
 
 summary(step_model)
 
@@ -94,6 +138,16 @@ for (i in 1:k) {
   fit = gamlss(formula = HeartDisease ~ ., family = BI(mu.link = probit), data = heart_step[!groupi,])
   predictvalsGLM[groupi] = predict(object = fit, new_data = heart_step[groupi,], type = "response")
 }
+
+
+library(pROC)
+
+PROC_obj <- roc(predictor = predictvalsGLM, response=dt$HeartDisease,
+                       curve=TRUE)
+plot(PROC_obj)
+
+hist(predictvalsGLM)
+
 
 
 #Find the best threshold value
