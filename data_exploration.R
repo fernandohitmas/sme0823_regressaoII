285a3e431019c245e98ab735a821120f93b62630
fernandohitmas <fernando.masumoto@gmail.com>
2023-11-29 15:42:59 -0300

    Initial investigation

--- data_exploration.R
+++ data_exploration.R
@@ -5,20 +5,15 @@ library(ggplot2)
 dt <- fread("./data/alb_homes.csv")
 
 # Verifica colunas que possuem NA
-# "yearbuilt": nao tem necessidade de se manter ja que a variavel de idade do imovel
-# "halfbath" : 
+# "yearbuilt": nao tem necessidade de se manter ja que a variavel de idade do imovel indica a mesma coisa
+# "halfbath" : serao removidos os na, uma vez que a quantidade eh muito pequena, 4 entre 3025
 colnames(dt)[ apply(dt, 2, anyNA)]
-dt <- dt[,-1]
 
-table(dt[, list(fullbath,halfbath)])
-
-
-
-ggplot(data = dt)+
-  geom_point(aes(x=fullbath, y=halfbath))
-
+# linhas que possuem NA
 dt[!complete.cases(dt), ]
 
+# Remocao de NA e remocao da coluna yearbuilt
+dt <- dt[,-1]
 dt <- na.omit(dt)
 
 # Numero de variveis independentes (p) e numero de linhas (n)
